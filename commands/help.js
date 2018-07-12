82bc3ad23b4035ca3c106d8909898cc46cac3dbc
sadguy <40127132+SadGuyComplete@users.noreply.github.com>
2018-07-12 11:29:45 -0400

    Delete help.js

--- commands/help.js
+++
@@ -1,12 +0,0 @@
-const Discord = require("discord.js");
-
-
-module.exports.run = async (bot, message, args) => {
-      let helpembed = new Discord.RichEmbed()      .setTitle('List Of Commands | prefix b; | Help List ')      .setDescription('There is available commands for this bot on list!')      .addField(':newspaper: INFO', '7 Commands open \n``help info``')      .addField(':tada: FUN', '2 Commands open \n``help fun``')      .setColor('RANDOM')      .setFooter(`Requested by ${message.author.tag} | Help list`)
-
-  return message.channel.send(helpEmbed);
-}
-
-module.exports.help = {
-  name: "help"
-}
