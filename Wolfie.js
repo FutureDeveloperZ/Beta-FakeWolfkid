48772c41a698c710ef029d81c75397632a65899b
Wolfie <Wolfkid200444@gmail.com>
2018-07-23 13:20:00 -0400

    Add files via upload

--- Wolfie.js
+++ Wolfie.js
@@ -58,13 +58,16 @@ var S = require('string');
    
    
       if (message.content === prefix + 'avatar') {     let user = message.mentions.users.first() || message.author;     let avatarembed = new Discord.RichEmbed()     .setTitle('Your Avatar!')     .setAuthor(`${user.username}`)     .setColor('RANDOM')     .setImage(message.author.avatarURL)      .setFooter(`Requested by ${message.author.tag}`)     return message.channel.send(avatarembed);   }      if (message.content === prefix + 'gayrate'){      let user = message.mentions.users.first() || message.author;      let gayembed = new Discord.RichEmbed()      .setAuthor(`${user.username}`)      .addField(`Gay Rate`, `You Are **${Math.floor(Math.random() * 100)}% Gay**! :gay_pride_flag:`)     .setColor('RANDOM')     .setFooter(`AH! GAYYYYYY | Requested by ${message.author.tag}`)      return message.channel.send(gayembed)   }   
-  if(message.content === prefix + "say"){
+  if(message.content === prefix + 'say') {
      let text = args.slice(1).join(" ");
      message.delete();
      message.channel.send(text);
    }
    
-   
+  if (message.content === prefix + '8ball') {
+        var rnd = Math.floor(Math.random() * config.eightball.length);
+        message.reply(config.eightball[rnd]);
+   }
    
    
    
