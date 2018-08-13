4447fff7be4af6ee61c503d8a3fa2c7423ae6ac0
Donald J. Trump <Ilemy939@gmail.com>
2018-08-13 16:59:45 +0300

    Update Wolfie.js

--- Wolfie.js
+++ Wolfie.js
@@ -21,7 +21,6 @@ client.on('message', async message => {
 if (!message.content.startsWith(prefix) || message.author.bot) return;
 
 const args = message.content.slice(prefix.length).split(' ');
-
 const command = args.shift().toLowerCase();
 
 
